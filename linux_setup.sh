ecd2fc543c6c2d0431595646d5f8143c2b475097
Frances Morales <fmorales@francesmorales.com>
2016-04-07 18:08:00 -0700

    Linux setup script

---
+++ linux_setup.sh
@@ -0,0 +1,4 @@
+sudo apt-get install git
+(cd /tmp && git clone --depth 1 https://github.com/twolfson/sexy-bash-prompt && cd sexy-bash-prompt && make install) && source ~/.bashrc
+sudo apt-get install ruby1.9.1
+sudo gem install bundler
